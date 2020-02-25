77cf62d4e4c2d8b0a428ad7d8b6315f468b4329b
Jesse Farmer <jesse@20bits.com>
2020-02-25 14:34:07 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# JavaScript Template
+# Express Sandbox
 
+Let's build our first dynamic web application! We'll be using the [Express][url-express] framework, a lightweight way to write web application
+
+## Contents <!-- omit in toc -->
+
+- [What A Web Application Is](#what-a-web-application-is)
+- [Getting Started](#getting-started)
+- [Errors And Restarting The Web Server](#errors-and-restarting-the-web-server)
+- [Iterations](#iterations)
+  - [[v0] Explore](#v0-explore)
+  - [[v1] Greeting Page (URL Only)](#v1-greeting-page-url-only)
+  - [[v2] Greeting Page (Form)](#v2-greeting-page-form)
+  - [[v3] Your Own Page](#v3-your-own-page)
+
+## What A Web Application Is
+
+Originally, the web was invented to share [hypertext documents][wiki-hypertext], mostly among academics. People would write [HTML files][wiki-html] — HTML stands for **H**yper**t**ext **M**arkup **L**anguage — and serve them up using a simple document server (called a web server). Users would navigate between web servers using a browser, a special piece of software that could turn HTML into something graphical and clickable.
+
+Web browsers and web servers communicate using [HTTP][wiki-http], the **H**yper**t**ext **T**ransfer **P**rotocol.
+
+A web application is a program that listens for incoming HTTP requests and responds with a programmatically-generated HTML document.
+
+## Getting Started
+
+1. Fork this repository
+1. Use `git clone` to get a local copy of your fork
+1. Run `npm install` inside the project directory
+1. Run `npm start` to start the web application
+1. Navigate to <http://localhost:3000>
+1. Start on the [iterations](#iterations)
+
+**Remember**, you have to run `npm install` before any of this will work.
+
+## Errors And Restarting The Web Server
+
+1. You can restart the web server by pressing `Ctrl+C` in the terminal where the web server is running.
+1. If you see an error involving `EADDRINUSE` that means you're already running the web server somewhere. You have to kill that copy of the server.
+
+   To do that, run the following:
+
+   ```console
+   ps aux | grep app.js
+   ```
+
+   You should see a line that contains `node app.js`, like this:
+
+   ```console
+   jesse            40313   0.0  0.1  4571396  24564 s000  S+    8:26AM   0:00.15 node app.js
+   ```
+
+   The second column is the *process ID*. Run the following to kill that process:
+
+   ```console
+   kill 40313
+   ```
+
+## Iterations
+
+### [v0] Explore
+
+Before you write any code, get the web server running and visit <http://localhost:3000>. Click around and explore. When you visit a page, look at `app.js` and see what the web application is doing.
+
+Pay attention to how the web application...
+
+- Knows which block of code to run
+- Creates the HTML document
+- Sends the HTML document back to the browser
+
+### [v1] Greeting Page (URL Only)
+
+Make sure you understand how the web application handles `/bake` before attempting this.
+
+- [ ] As a user, when I visit `/greet?name=WXYZ`, I see a message greeting `WXYZ`. `WXYZ` could be any string, e.g., `/greet?name=Jesse`, `/greet?name=Wilfred`.
+- [ ] As a user, when I visit `/greet`, I see a message telling me there's no greeting to give.
+
+### [v2] Greeting Page (Form)
+
+(Again, look at the form on `/bake`) to see how it works.
+
+- [ ] As a user, when I visit `/greet` (with or without parameters), I see a form that allows me to enter a name and submit to see a new greeting.
+
+### [v3] Your Own Page
+
+Add your own page that has dynamic behavior. Servers can read/write to files, so maybe think of information you want to remember between page loads.
+
+For example, you could read the contents of a `todos.txt` and display a list on the page.
+
+Or, you could display the current time.
+
+Or, you could keep track of how many people visited the page.
+
+[url-express]: https://expressjs.com/
+[wiki-hypertext]: https://en.wikipedia.org/wiki/Hypertext
+[wiki-html]: https://en.wikipedia.org/wiki/HTML
+[wiki-http]: https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol
---
+++ app.js
@@ -0,0 +1,169 @@
+// Node's standard path module
+// See https://nodejs.org/api/path.html
+let path = require('path');
+
+// The Express web application framework
+// See http://expressjs.com/
+let express = require('express');
+
+// Library for nicer logging of HTTP requests
+// See https://github.com/expressjs/morgan
+let logger = require('morgan');
+
+let app = express();
+
+// Tell Express to load static files from the public/ directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Tell Express to log HTTP requests in the 'dev' format
+// See the Morgan documentation for what that looks like
+app.use(logger('dev'));
+
+// A helper we wrote to capitalize strings
+let capitalize = require('./lib/capitalize');
+
+// The overall layout remains the same between pages, so we use
+// this helper function to wrap our page-specific content in the layout.
+function getLayoutHTML(content) {
+  // Template strings can span multiple lines, making them
+  // well-suited for, well, acting as templates.
+  let html = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Hello, world!</title>
+
+        <link rel="stylesheet" href="/css/normalize.css">
+        <link rel="stylesheet" href="/css/main.css">
+      </head>
+      <body>
+        <section id="content">
+          ${content}
+        </section>
+      </body>
+    </html>
+  `;
+
+  return html;
+}
+
+app.get('/', (request, response) => {
+  let appDescription = `
+    <p>This application is meant to highlight some of the essential ideas in web development.  You should already be familiar with some basic HTML and know how to create a standalone, static HTML document that you can open with your browser.</p>
+    <p>At its core, every web application has the same job: listen for incoming requests, generate an HTML document, and send that document back to the browser.  Unlike a static HTML document, a web application can respond to dynamic, user-supplied information.  If a static HTML document is like a vending machine then a web application is like a restaurant with a real, live chef.</p>
+  `;
+
+  let content = `
+    <h1>Hello, World!</h1>
+    ${appDescription}
+    <p>Start exploring by clicking the links below.</p>
+    <h2>Static Pages</h2>
+    <p>These pages don't respond to any user-supplied data.</p>
+    <ul>
+    <li><a href="/waffles">Plain ol' waffles</a></li>
+    <li><a href="/waffles/chocolate">Chocolate waffles</a></li>
+    </ul>
+
+    <h2>Dynamic Pages</h2>
+    <p>These pages <em>do</em> respond to user-supplied data.  When you visit the links below, look at the URL.  Everything after the <code>?</code> character is called the _query string_ and it's one place we can supply dynamic information. </p>
+    <ul>
+    <li><a href="/waffles/custom?type=banana">Banana waffles</a></li>
+    <li><a href="/waffles/custom?type=blueberry">Blueberry waffles</a></li>
+    <li><a href="/waffles/custom?type=nutella">Nutella waffles</a></li>
+    <li><a href="/bake?baked_good=cookies&count=10">Bake 10 cookies</a></li>
+    <li><a href="/bake?baked_good=loaves+of+bread&count=5">Bake 5 loaves of bread</a> — notice how we represent spaces in the URL.</li>
+    <li><a href="/bake?baked_good=cupcakes&count=1138">Bake 1138 cupcakes</a></li>
+    </ul>
+  `;
+
+  let pageHtml = getLayoutHTML(content);
+
+  response.send(pageHtml);
+});
+
+app.get('/waffles', (request, response) => {
+  let content = `
+    <h1>Waffle Time!</h1>
+    <p>Everybody loves waffles.</p>
+    <p><a href="/">Back to the homepage</a></p>
+  `;
+
+  response.send(getLayoutHTML(content));
+});
+
+app.get('/waffles/chocolate', (request, response) => {
+  let content = `
+    <h1>Chocolate Waffle time!</h1>
+    <p>Chocolate waffles: better than regular waffles.</p>
+    <p><a href="/">Back to the homepage</a></p>
+  `;
+
+  response.send(getLayoutHTML(content));
+});
+
+app.get('/waffles/custom', (request, response) => {
+  let waffleType = capitalize(request.query.type);
+
+  let content = `
+    <h1>${waffleType} Waffle time!</h1>
+    <p>${waffleType} waffles: better than regular waffles.</p>
+    <p>Change the value of the <code>type</code> parameter in the URL and see what happens.</p>
+    <p><a href="/">Back to the homepage</a></p>
+  `;
+
+  response.send(getLayoutHTML(content));
+});
+
+// Visit, e.g., /bake?baked_good=waffles&count=20
+app.get('/bake', (request, response) => {
+  let count = Number.parseInt(request.query.count);
+  let bakedGood = request.query.baked_good;
+
+  let content = `
+    <h1>Time To Bake ${capitalize(bakedGood)}!</h1>
+    <p>
+      <a href='/'>Back to the homepage</a>
+    </p>
+    <p>
+      Change the value of the <code>count</code> and <code>baked_good</code> parameters in the URL and see what happens!
+    </p>
+    <form method="GET" action="/bake">
+      <div class="form-section">
+        <label for="baked_good">Baked good:</label>
+        <input type="text" name="baked_good" id="baked_good" required>
+      </div>
+      <div class="form-section">
+        <label for="count">Number of baked goods:</label>
+        <input type="number" name="count" id="count" required>
+      </div>
+      <div class="form-section">
+        <input type="submit" value="Let's bake!">
+      </div>
+    </form>
+    <h2>Our ${capitalize(bakedGood)}</h2>
+    <p>
+      I like ${bakedGood}, too.  Let's bake ${count} of them.
+    </p>
+  `;
+
+  content += '<ul>';
+
+  for(let i = 1; i <= count; i++) {
+    content += `<li>${bakedGood} number ${i}</li>`;
+  }
+
+  content += '</ul>';
+
+  response.send(getLayoutHTML(content));
+});
+
+let SERVER_PORT = process.env.PORT || 3000;
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Listening on port ${SERVER_PORT}...`);
+  console.log('Visit this URL in your browser to see the web app:');
+  console.log();
+  console.log(`    http://localhost:${SERVER_PORT}`);
+  console.log();
+});
